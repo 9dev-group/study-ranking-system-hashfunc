2f1d8e4ee39186e65512238028ab8d7c93ea72d3
hashfunc <root@hashfunc.io>
2025-03-22 20:22:20 +0900

    Set up spring project

---
+++ src/main/kotlin/_9dev/study/hashfunc/HashfuncApplication.kt
@@ -0,0 +1,11 @@
+package _9dev.study.hashfunc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HashfuncApplication
+
+fun main(args: Array<String>) {
+	runApplication<HashfuncApplication>(*args)
+}
---
+++ src/test/kotlin/_9dev/study/hashfunc/HashfuncApplicationTests.kt
@@ -0,0 +1,13 @@
+package _9dev.study.hashfunc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HashfuncApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
