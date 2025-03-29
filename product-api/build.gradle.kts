a4410f9e7154d8b46293a5351e948aee07f48e7d
hashfunc <root@hashfunc.io>
2025-03-29 23:53:18 +0900

    feat(product-api): Set logging configurations

--- product-api/build.gradle.kts
+++ product-api/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     }
     implementation("org.springframework.boot:spring-boot-starter-undertow")
 
+    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
     implementation("org.postgresql:postgresql:42.7.5")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
