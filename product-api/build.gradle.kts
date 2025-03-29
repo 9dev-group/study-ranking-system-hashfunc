92bac9b2f1ae59568f47775160108a6539745c9e
hashfunc <root@hashfunc.io>
2025-03-29 21:07:30 +0900

    feat(product-api): Change web server to undertow

--- product-api/build.gradle.kts
+++ product-api/build.gradle.kts
@@ -2,7 +2,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude("org.springframework.boot", "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-undertow")
 
     implementation("org.postgresql:postgresql:42.7.5")
 
