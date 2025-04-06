cecd042757d55a8da2838752679060b39512fde2
hashfunc <root@hashfunc.io>
2025-04-06 19:12:24 +0900

    refactor(infrastructure): Migrate vector to cdk8s

--- infrastructure/kubernetes/src/main/kotlin/_9dev/study/infrastructure/kubernetes/MainChart.kt
+++ infrastructure/kubernetes/src/main/kotlin/_9dev/study/infrastructure/kubernetes/MainChart.kt
@@ -1,10 +1,15 @@
 package _9dev.study.infrastructure.kubernetes
 
+import _9dev.study.infrastructure.kubernetes.vector.Vector
 import org.cdk8s.App
 import org.cdk8s.Chart
 
 
-class MainChart(scope: software.constructs.Construct, id: String) : Chart(scope, id)
+class MainChart(scope: software.constructs.Construct, id: String) : Chart(scope, id) {
+    init {
+        Vector(this, "vector")
+    }
+}
 
 fun main() {
     val app = App()
---
+++ infrastructure/kubernetes/src/main/kotlin/_9dev/study/infrastructure/kubernetes/vector/Vector.kt
@@ -0,0 +1,9 @@
+package _9dev.study.infrastructure.kubernetes.vector
+
+import org.cdk8s.Chart
+
+class Vector(scope: software.constructs.Construct, id: String) : Chart(scope, id) {
+    init {
+        VectorAgent(this, "vector-agent")
+    }
+}
