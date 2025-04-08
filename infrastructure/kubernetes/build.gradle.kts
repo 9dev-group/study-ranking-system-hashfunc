f272efe9ab172bfe2ff9093a998933fc5bb4bb54
hashfunc <root@hashfunc.io>
2025-04-08 21:28:36 +0900

    feat(infrastructure): Load values from yaml file

--- infrastructure/kubernetes/build.gradle.kts
+++ infrastructure/kubernetes/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.3")
     implementation("org.cdk8s:cdk8s:2.69.60")
     implementation("software.constructs:constructs:10.4.2")
 }
