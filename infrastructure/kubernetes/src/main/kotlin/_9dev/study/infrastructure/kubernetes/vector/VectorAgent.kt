f48c7cc9a6f6a81fdf0c867eb1777eaf473e1df7
hashfunc <root@hashfunc.io>
2025-04-08 21:28:36 +0900

    feat(infrastructure): Load values from yaml file

--- infrastructure/kubernetes/src/main/kotlin/_9dev/study/infrastructure/kubernetes/vector/VectorAgent.kt
+++ infrastructure/kubernetes/src/main/kotlin/_9dev/study/infrastructure/kubernetes/vector/VectorAgent.kt
@@ -1,14 +1,12 @@
 package _9dev.study.infrastructure.kubernetes.vector
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.cdk8s.Chart
 import org.cdk8s.Helm
 
 class VectorAgent(scope: software.constructs.Construct, id: String) : Chart(scope, id) {
-    val bootstrapServers
-        get() = listOf(
-            "main-cluster-main-pool-0.main-cluster-kafka-brokers.default.svc.cluster.local:9092"
-        ).joinToString(",")
-
     init {
         Helm.Builder.create(this, "vector")
             .repo("https://helm.vector.dev")
@@ -19,41 +17,10 @@ class VectorAgent(scope: software.constructs.Construct, id: String) : Chart(scop
             .build()
     }
 
-    val values
-        get() = mapOf(
-            "role" to "Agent",
-            "service" to service,
-            "customConfig" to mapOf(
-                "dataDir" to "/vector-data-dir",
-                "sources" to sources,
-                "sinks" to sinks,
-            ),
-        )
-
-    val service
-        get() = mapOf(
-            "enabled" to false,
-        )
-
-    val sources
-        get() = mapOf(
-            "kubernetes_log_source" to mapOf(
-                "type" to "kubernetes_logs",
-            ),
-        )
-
-    val sinks
-        get() = mapOf(
-            "kubernetes_log_sink" to mapOf(
-                "type" to "kafka",
-                "input" to listOf(
-                    "kubernetes_log_source",
-                ),
-                "bootstrap_servers" to bootstrapServers,
-                "topic" to "infra.kubernetes.vector.logs.v1",
-                "encoding" to mapOf(
-                    "codec" to "json",
-                ),
-            ),
-        )
+    val values: Map<String, Any>
+        get() {
+            val objectMapper = ObjectMapper(YAMLFactory()).registerKotlinModule()
+            this.javaClass.classLoader.getResourceAsStream("vector/agent/values.yaml")
+                .use { return objectMapper.readValue(it, Map::class.java) as Map<String, Any> }
+        }
 }
