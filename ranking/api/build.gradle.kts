415de7a7a4d03cb6514eb2c404c9e72c5b9016dc
hashfunc <root@hashfunc.io>
2025-04-13 22:08:06 +0900

    feat(ranking): Add product ranking api

--- ranking/api/build.gradle.kts
+++ ranking/api/build.gradle.kts
@@ -1,14 +1,32 @@
+repositories {
+    maven {
+        url = uri("https://packages.confluent.io/maven/")
+    }
+    maven {
+        url = uri("https://confluent-snapshots.public.confluent.io/")
+    }
+}
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web") {
         exclude("org.springframework.boot", "spring-boot-starter-tomcat")
     }
     implementation("org.springframework.boot:spring-boot-starter-undertow")
 
+    implementation("io.confluent.ksql:ksqldb-api-client:7.9.0") {
+        exclude("org.slf4j", "slf4j-reload4j")
+    }
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.bootBuildImage {
-    createdDate = "now"
+jib {
+    from {
+        image = "gcr.io/distroless/java21-debian12"
+    }
+    to {
+        image = project.name
+        tags = setOf(project.version.toString())
+    }
 }
---
+++ ranking/api/src/main/kotlin/_9dev/study/ranking/api/model/RankingProduct.kt
@@ -0,0 +1,6 @@
+package _9dev.study.ranking.api.model
+
+data class RankingProduct(
+    val path: String,
+    val count: Long,
+)
