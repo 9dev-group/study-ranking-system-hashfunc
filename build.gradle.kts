f7005121da58c0a91f8fbf2cc782b59f8b488a3d
hashfunc <root@hashfunc.io>
2025-05-11 18:23:59 +0900

    refactor: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("jvm").version("1.9.25")
+    kotlin("plugin.spring").version("1.9.25")
+    kotlin("plugin.jpa").version("1.9.25")
 
-    id("org.springframework.boot") version "3.4.4"
-    id("io.spring.dependency-management") version "1.1.7"
-    id("com.google.cloud.tools.jib") version "3.4.5"
+    id("org.springframework.boot").version("3.4.4")
+    id("io.spring.dependency-management").version("1.1.7")
+    id("com.google.cloud.tools.jib").version("3.4.5")
     id("org.jlleitschuh.gradle.ktlint").version("12.2.0")
 }
 
@@ -19,13 +19,17 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin = "kotlin")
-    apply(plugin = "kotlin-spring")
-    apply(plugin = "kotlin-jpa")
-    apply(plugin = "org.springframework.boot")
-    apply(plugin = "io.spring.dependency-management")
-    apply(plugin = "com.google.cloud.tools.jib")
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply {
+        plugin("kotlin")
+        plugin("kotlin-spring")
+        plugin("kotlin-jpa")
+
+        plugin("io.spring.dependency-management")
+        plugin("org.springframework.boot")
+
+        plugin("com.google.cloud.tools.jib")
+        plugin("org.jlleitschuh.gradle.ktlint")
+    }
 
     kotlin {
         compilerOptions {
